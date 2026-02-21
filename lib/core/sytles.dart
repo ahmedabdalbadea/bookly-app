05bb4a8ae635cb68afec8901da60b41d62a5b716
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-02-21 22:57:45 +0200

    splash feature done

---
+++ lib/core/sytles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: Colors.white70,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+}
--- lib/features/splash/view/widgets/splash_view_body.dart
+++ lib/features/splash/view/widgets/splash_view_body.dart
@@ -1,12 +1,53 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with TickerProviderStateMixin {
+  late AnimationController animationController;
+
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 2),
+      end: Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(kLogo),
+          AnimatedBuilder(
+            animation: slidingAnimation,
+            child: Text("Reading Free Book"),
+            builder: (context, child) {
+              return SlideTransition(
+                position: slidingAnimation,
+                child: Text("Reading Free Book"),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
